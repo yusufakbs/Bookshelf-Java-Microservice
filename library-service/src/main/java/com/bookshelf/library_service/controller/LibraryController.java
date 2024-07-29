4f1fdd691ea9a545ffe5138cf94bb6e953e453c7
yusufakbs <akbasyusuuf@gmail.com>
2024-07-29 17:28:19 +0300

    feat: Added Spring Cloud Config Server for centralized configuration management
                   - Added Spring Cloud Config Server dependencies to pom.xml
                   - Enabled Config Server in main application class with @EnableConfigServer

--- library-service/src/main/java/com/bookshelf/library_service/controller/LibraryController.java
+++ library-service/src/main/java/com/bookshelf/library_service/controller/LibraryController.java
@@ -3,8 +3,11 @@ package com.bookshelf.library_service.controller;
 import com.bookshelf.library_service.dto.AddBookRequest;
 import com.bookshelf.library_service.dto.LibraryDto;
 import com.bookshelf.library_service.service.LibraryService;
+import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +15,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RefreshScope
 @RequestMapping("/v1/library")
 public class LibraryController {
+
+    @Value("${library.service.count}")
+    private Integer count;
 
     Logger logger = LoggerFactory.getLogger(LibraryController.class);
 
@@ -46,6 +53,12 @@ public class LibraryController {
     public ResponseEntity<List<String>> getAllLibraries(){
         return ResponseEntity.ok(libraryService.getAllLibraries());
     }
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount(){
+        return ResponseEntity.ok("Library service count : " + count);
+    }
+
 
 
 
